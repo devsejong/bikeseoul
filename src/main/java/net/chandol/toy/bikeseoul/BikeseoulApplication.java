ab331a5bb5e9d021dc7d51d19cb104fecc1247fd
sjpark <devsejong@gmail.com>
2017-10-24 18:38:39 +0900

    Initial commit

---
+++ src/main/java/net/chandol/toy/bikeseoul/BikeseoulApplication.java
@@ -0,0 +1,12 @@
+package net.chandol.toy.bikeseoul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeseoulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikeseoulApplication.class, args);
+	}
+}
