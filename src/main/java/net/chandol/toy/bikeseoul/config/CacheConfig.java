6b8e41883f8007390b3fe7af596cecc8ea705670
sjpark <devsejong@gmail.com>
2017-10-25 18:38:02 +0900

    .

---
+++ src/main/java/net/chandol/toy/bikeseoul/config/CacheConfig.java
@@ -0,0 +1,12 @@
+package net.chandol.toy.bikeseoul.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableCaching
+@EnableScheduling
+public class CacheConfig {
+
+}
--- src/main/java/net/chandol/toy/bikeseoul/controller/BikeSeoulController.java
+++ src/main/java/net/chandol/toy/bikeseoul/controller/BikeSeoulController.java
@@ -1,17 +1,32 @@
 package net.chandol.toy.bikeseoul.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+@Slf4j
 @RestController
 public class BikeSeoulController {
-    @Autowired RestTemplate restTemplate;
+    private static final String BIKE_STATUS = "BIKE_STATUS";
+
+    @Autowired
+    RestTemplate restTemplate;
 
     @GetMapping("/status")
+    @Cacheable(value=BIKE_STATUS)
     public String getStatus(){
         String url = "https://www.bikeseoul.com/app/station/getStationRealtimeStatus.do";
         return restTemplate.getForObject(url, String.class);
+    }
+
+    @CacheEvict(value = BIKE_STATUS)
+    @Scheduled(fixedDelay = 30 * 1000 ,  initialDelay = 500)
+    public void reportCacheEvict() {
+        log.debug("Cache Flushed " + System.currentTimeMillis());
     }
 }
